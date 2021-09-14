c25c69430f660b3e322a826af873554bc697402c
Virendra patel <patelvirendra62@gmail.com>
2021-09-14 08:58:35 +0530

    multiple exports

---
+++ 03_modules/02/index.js
@@ -0,0 +1,9 @@
+// const student = { name: "virendra", age: 23 };
+// const { name, age } = student;
+// console.log(name, age);
+
+const { sum, multiply, substract } = require("./math");
+
+console.log(sum(12, 10));
+console.log(multiply(12, 10));
+console.log(substract(12, 10));
---
+++ 03_modules/02/math.js
@@ -0,0 +1,9 @@
+const sum = (number1, number2) => number1 + number2;
+const multiply = (number1, number2) => number1 * number2;
+const substract = (number1, number2) => number1 - number2;
+
+// module.exports = {}
+
+module.exports.sum = sum; //{ sum : funtion}
+module.exports.multiply = multiply; // { sum : funtion , multiply : funtion }
+module.exports.substract = substract; // { sum : funtion , multiply : funtion , substract : funtion  }
